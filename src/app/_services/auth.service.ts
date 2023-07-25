516941f549ff6817031650ea92f4116d192654c3
Tsepang Maseti <64875578+Tkmaseti@users.noreply.github.com>
2023-07-26 00:25:32 +0200

    Update auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { User } from 'src/app/_interface/users';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const AUTH_API = 'https://fine-backend-bfrt.onrender.com';
+const AUTH_API = 'https://fine-backend-bfrt.onrender.com/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
