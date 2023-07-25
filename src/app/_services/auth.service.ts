986219b58dd90fbda78e7520116cbbe760bc45fb
Tsepang Maseti <64875578+Tkmaseti@users.noreply.github.com>
2023-07-26 00:27:07 +0200

    Update auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -15,7 +15,7 @@ const httpOptions = {
 export class AuthService {
   constructor(private http: HttpClient) { }
   login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(AUTH_API + '/signin', {
       username, password
     },
       httpOptions);
